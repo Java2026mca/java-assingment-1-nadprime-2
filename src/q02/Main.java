f81c42cab9bf45d599c7e282a1b958e11d3c68ff
Nadeem Ahmad <103782863+nadprime@users.noreply.github.com>
2026-04-07 22:50:40 +0530

    Add Pascal's Triangle printing functionality
    Implemented logic to print Pascal's Triangle for n rows.

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,21 +1,21 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        // TODO: Print Pascal's Triangle for n rows
-        //       Values on each row separated by single space
-        //       No leading or trailing spaces
-        //
-        // Input: 5
-        // Output:
-        // 1
-        // 1 1
-        // 1 2 1
-        // 1 3 3 1
-        // 1 4 6 4 1
-
+        
+        long[] row = new long[n];
+        row[0] = 1;
+        
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j <= i; j++) {
+                if (j > 0) sb.append(' ');
+                sb.append(row[j]);
+            }
+            System.out.println(sb.toString());
+            
+            for (int j = i; j >= 1; j--) row[j] += row[j - 1];
+        }
     }
 }
