83d8a54d989ff53598d45b384b02e50924c45acb
Nadeem Ahmad <103782863+nadprime@users.noreply.github.com>
2026-04-07 23:00:25 +0530

    Refactor main method for Pascal's triangle output
    Refactor the main method to compute and print Pascal's triangle without using an array.

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,18 +1,18 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long[] row = new long[n];
-        row[0] = 1;
+
         for (int i = 0; i < n; i++) {
-            StringBuilder sb = new StringBuilder();
+            int val = 1;
             for (int j = 0; j <= i; j++) {
-                if (j > 0) sb.append(' ');
-                sb.append(row[j]);
+                System.out.print(val);
+                if (j < i) System.out.print(" ");
+                val = val * (i - j) / (j + 1);
             }
-            System.out.println(sb);
-            for (int j = i; j >= 1; j--) row[j] = row[j] + row[j-1];
+            if (i < n - 1) System.out.println();
         }
     }
 }
