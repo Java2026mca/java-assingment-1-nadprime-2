8ccf3ad5076005ac6d78f37318f64ef27517ac13
Nadeem Ahmad <103782863+nadprime@users.noreply.github.com>
2026-04-07 22:56:23 +0530

    Refactor row update logic and improve output

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -3,18 +3,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
         long[] row = new long[n];
         row[0] = 1;
-        
         for (int i = 0; i < n; i++) {
             StringBuilder sb = new StringBuilder();
             for (int j = 0; j <= i; j++) {
                 if (j > 0) sb.append(' ');
                 sb.append(row[j]);
             }
-            System.out.println(sb.toString());
-            for (int j = i; j >= 1; j--) row[j] += row[j-1];
+            System.out.println(sb);
+            for (int j = i + 1; j > 0; j--) row[j] = row[j] + row[j-1];
         }
     }
 }
